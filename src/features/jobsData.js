b0042be227b2b9ffe0c8a4fbfeabac8dd3e67cbd
MargaretTechworld <margarettechworld@gmail.com>
2025-06-20 18:06:18 +0000

    Add redux styles and data

---
+++ src/features/jobsData.js
@@ -0,0 +1,23 @@
+export const jobs = [
+  {
+    id: 1,
+    title: 'HR Officer',
+    location: 'Freetown',
+    department: 'HR',
+    salary: 2500,
+  },
+  {
+    id: 2,
+    title: 'Engineer',
+    location: 'Bo',
+    department: 'Engineering',
+    salary: 3500,
+  },
+  {
+    id: 3,
+    title: 'Finance Analyst',
+    location: 'Makeni',
+    department: 'Finance',
+    salary: 3000,
+  },
+];
