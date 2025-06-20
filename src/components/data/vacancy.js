46b16c4224cb6f1eaa224c4ce51fbe2d7b3d8f52
MargaretTechworld <margarettechworld@gmail.com>
2025-06-20 18:08:46 +0000

    Add data

---
+++ src/components/data/vacancy.js
@@ -0,0 +1,26 @@
+const vacancy = [
+  {
+    id: 1,
+    PostingDate: '20-10-25',
+    JobTitle: 'Software Engineer',
+    Type: 'Full-time',
+    Description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Inscelerisque libero at eros fringilla, sit amet pulvinar lectus accumsan. Nullam lobortis, nunc eget varius faucibus, dui quam tincidunt risus, at blandit libero purus vitae justo...',
+    Location: 'Keneme',
+    Departmet: 'Technology',
+    ClosingDate: '20-11-25',
+  },
+  {
+    id: 1,
+    PostingDate: '20-10-25',
+    JobTitle: 'Project Engineer',
+    Type: 'Full-time',
+    Description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Inscelerisque libero at eros fringilla, sit amet pulvinar lectus accumsan. Nullam lobortis, nunc eget varius faucibus, dui quam tincidunt risus, at blandit libero purus vitae justo...',
+    Location: 'Keneme',
+    Departmet: 'Technology',
+    ClosingDate: '20-11-25',
+  },
+];
+
+export default vacancy;
